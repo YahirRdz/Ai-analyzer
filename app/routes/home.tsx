ec5da90b90d6399fd87f2e0ee0b2e6c3621cce27
YahirRdz <yajosh03@gmail.com>
2025-08-02 22:07:13 -0600

    Autentication and into to Puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@
  import {resumes} from "~/routes/constants";
  import {callback} from "fdir/dist/api/async";
  import ResumeCard from "~/components/ResumeCard";
+ import {usePuterStore} from "~/lib/puter";
+ import {useLocation, useNavigate} from "react-router";
+ import {useEffect} from "react";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -13,12 +16,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
+
+
   return <main className="bg-amber-100">
     <Navbar />
+
     <section className="main-section py-16">
       <div className="page-heading">
         <h1>Track your Applications & Resume Ratings</h1>
         <h2>Review your submitions and check AI-powered feedback</h2>
+
       </div>
     {resumes.length > 0 && (
     <div className="resumes-section">
---
+++ app/routes/upload.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Upload = () => {
+    return (
+        <div>
+            Upload profile
+        </div>
+    );
+};
+
+export default Upload;
